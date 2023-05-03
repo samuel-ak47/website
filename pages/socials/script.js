95e9edf22e30f46e8e96e01e0096c4892f802894
LionPlayz <lionplayz12@gmail.com>
2023-05-03 15:17:35 -0400

    updated most pages

--- pages/socials/script.js
+++ pages/socials/script.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-    document.getElementById("MainVideovid").preload='auto'
-    document.getElementById("show_modal").click()
+    document.getElementById("video").preload='auto'
+    document.getElementById("show-modal").click()
 }
 click=0
 window.onclick = function() {
     if (click==1){
-        document.getElementById("MainVideovid").play()
-        document.getElementById("close_modal").click()
-        document.getElementById("MainVideovid").style.display='';
-        document.getElementById("MainVideodesc").style.display='';
-        document.getElementById("MainVideovid").scrollIntoView();
+        document.getElementById("video").play()
+        document.getElementById("video").style.display='';
+        document.getElementById("msg").style.display='';
+        document.getElementById("msg").scrollIntoView();
+        document.getElementById("cookie-modal").style.display="none";
     }
     click+=1
 }
